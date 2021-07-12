7fb86009b1a441d58425935477ce109d8f4e9333
Bwz3r <68409068+bwz3rbot@users.noreply.github.com>
2021-07-12 05:07:00 -0700

    tickerlist

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const axios = require('axios').default;
 const cheerio = require('cheerio');
-
-(async () => {
+module.exports = async () => {
     const res = await axios.get('https://stockanalysis.com/stocks/');
     const $ = cheerio.load(res.data);
     const list = $('ul.no-spacing');
     const li = list.find('li');
+    const tickerList = [];
     li.each((i, el) => {
-        console.log($(el).text());
+        const text = $(el).text().split(' - ');
+        console.log(text);
+        const ticker = text[0];
+        const company = text[1];
+        tickerList.push({
+            ticker,
+            company
+        });
     });
-})();
+    return tickerList;
+}
